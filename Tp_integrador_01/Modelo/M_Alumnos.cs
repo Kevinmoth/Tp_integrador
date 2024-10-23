8c566933baf21b700d7c34cca055dd11b5710917
A-c2024 <andrea.cabr4al@gmail.com>
2024-10-23 18:49:07 -0300

    y

---
+++ Tp_integrador_01/Modelo/M_Alumnos.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tp_integrador_01.Modelo
+{
+    internal class M_Alumnos
+    {
+    }
+}
